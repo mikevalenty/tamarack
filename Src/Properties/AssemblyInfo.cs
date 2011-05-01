7833d02ca1daaaa97c1180626fecd0897f363ef2
Michael Valenty <mike@valenty.com>
2011-05-01 08:36:59 -0700

    Updated nuget tags and assembly version number

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tamarack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A micro framework for implementing the Chain of Responsibility pattern in .NET. An essential tool in OO toolbox for transforming rigid procedural code into a composable Domain Specific Language.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Valenty")]
 [assembly: AssemblyProduct("Tamarack")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
