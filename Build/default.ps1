b3e600d085187ca885354a6289c097cea66bfc05
Christoph Keller <github@langalaxy.de>
2011-09-04 22:44:12 +0200

    Build extracts the version out of the .NET 4 build of Tamarack. No runtime patching is needed.

--- Build/default.ps1
+++ Build/default.ps1
@@ -6,16 +6,21 @@ properties {
     $nuget_path       = "$base_directory\packages\NuGet.CommandLine.1.4.20615.182\tools\"
     $solution_path    = "$base_directory\$project_name.sln"
     $nuspec_path      = "$base_directory\$project_name.nuspec"
-    $release_files    =  @("$project_name.dll");#@("Tamarack.dll", "Tamarack.xml" );
-    $version          = "1.0.0.0"
+    $release_files    =  @("$project_name.dll");#@("Tamarack.dll", "Tamarack.xml" );  
 }
 
 task Publish -depends package {
     
     # Publish the nuget package
+    # Todo
 }
 
 task Package -depends Default{
+    
+    # Get the version of the assembly, so no runtime patching is needed
+    Write-Host "Getting version of assembly"
+    $version = (Get-Command $build_directory\4.0\Tamarack.dll).FileVersionInfo.ProductVersion   
+    Write-Host "Current is $version"
     
     # Create the nuget package
     Exec { & $nuget_path\nuget.exe pack $nuspec_path -Version "$version" -OutputDirectory "$build_directory" }
@@ -24,9 +29,7 @@ task Package -depends Default{
 task default -depends Build
 
 task Clean -description "This task cleans up the build directory" {
-    
-    Write-Host "$solution_path "
-    
+       
     # Delete the build directory for a clean start    
     Remove-Item $build_directory -Force -Recurse -ErrorAction SilentlyContinue
 }
